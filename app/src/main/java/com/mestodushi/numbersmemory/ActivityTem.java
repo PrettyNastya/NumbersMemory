fa322c7e7315eb93472762fc09ec20c489ae6ee5
PrettyNastya <anastasia.mobile@yandex.ru>
2023-02-11 09:52:15 +0300

    добавлены кастомные цвета, наполнение главной активити, и вторая активити для выбора цветовой темы

---
+++ app/src/main/java/com/mestodushi/numbersmemory/ActivityTem.java
@@ -0,0 +1,15 @@
+package com.mestodushi.numbersmemory;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityTem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tem);
+    }
+
+}
