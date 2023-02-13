c85c587b952f1c2e24eae7a7a243b49bf335c57e
PrettyNastya <anastasia.mobile@yandex.ru>
2023-02-13 21:12:07 +0300

    зделала шоу текст вью

--- app/src/main/java/com/mestodushi/numbersmemory/ActivityTraining.java
+++ app/src/main/java/com/mestodushi/numbersmemory/ActivityTraining.java
@@ -10,22 +10,38 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ActivityTraining extends AppCompatActivity {
 
     int buttonTextSize = 18;
-    TextView textViewM_1;
+    TextView textViewShow_1, textViewShow_2, textViewShow_3, textViewShow_4, textViewShow_5;
     int exposition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traning);
-        textViewM_1 = findViewById(R.id.textViewM);
-        textViewM_1.setTypeface(null, Typeface.BOLD);
 
-        textViewM_1.setText("0");
+        textViewShow_1 = findViewById(R.id.textViewShow_1);
+        textViewShow_1.setTypeface(null, Typeface.BOLD);
+
+        textViewShow_2 = findViewById(R.id.textViewShow_2);
+        textViewShow_2.setTypeface(null, Typeface.BOLD);
+
+        textViewShow_3 = findViewById(R.id.textViewShow_3);
+        textViewShow_3.setTypeface(null, Typeface.BOLD);
+
+        textViewShow_4 = findViewById(R.id.textViewShow_4);
+        textViewShow_4.setTypeface(null, Typeface.BOLD);
+
+        textViewShow_5 = findViewById(R.id.textViewShow_5);
+        textViewShow_5.setTypeface(null, Typeface.BOLD);
+
+
+
+
+        textViewShow_1.setText("0");
 
     }
 
     public void button_0_click(View view) {
-        textViewM_1.setText("0");
+        textViewShow_1.setText("0");
 
     }
 
@@ -33,38 +49,38 @@ public class ActivityTraining extends AppCompatActivity {
     }
 
     public void button_9_click(View view) {
-        textViewM_1.setText("9");
+        textViewShow_1.setText("9");
     }
 
     public void button_8_click(View view) {
-        textViewM_1.setText("8");
+        textViewShow_1.setText("8");
     }
 
     public void button_7_click(View view) {
-        textViewM_1.setText("7");
+        textViewShow_1.setText("7");
     }
 
     public void button_6_click(View view) {
-        textViewM_1.setText("6");
+        textViewShow_1.setText("6");
     }
 
     public void button_5_click(View view) {
-        textViewM_1.setText("5");
+        textViewShow_1.setText("5");
     }
 
     public void button_4_click(View view) {
-        textViewM_1.setText("4");
+        textViewShow_1.setText("4");
     }
 
     public void button_3_click(View view) {
-        textViewM_1.setText("3");
+        textViewShow_1.setText("3");
     }
 
     public void button_2_click(View view) {
-        textViewM_1.setText("2");
+        textViewShow_1.setText("2");
     }
 
     public void button_1_click(View view) {
-        textViewM_1.setText("1");
+        textViewShow_1.setText("1");
     }
 }
